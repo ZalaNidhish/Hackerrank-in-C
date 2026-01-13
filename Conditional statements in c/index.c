65a189b0b49a0763993d4bd0245bc28711c861da
Nidhish Zala <nidhish1132007@gmail.com>
2026-01-13 20:03:10 +0530

    For loops in C

---
+++ Conditional statements in c/index.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+
+void numtoword(long num){
+    if(num == 1){
+        printf("one");
+    }
+    else if(num == 2){
+        printf("two");
+    }
+    else if(num == 3){
+        printf("three");
+    }
+    else if(num == 4){
+        printf("four");
+    }
+    else if(num == 5){
+        printf("five");
+    }
+    else if(num == 6){
+        printf("six");
+    }
+    else if(num == 7){
+        printf("seven");
+    }
+    else if(num == 8){
+        printf("eight");
+    }
+    else if(num == 9){
+        printf("nine");
+    }
+}
+
+
+int main(){
+
+    long num;
+    long* p = &num;
+    scanf("%ld", p);
+    
+    if(*p>9){
+        printf("Greater than 9");
+    }else if(*p>=1){
+        numtoword(*p);
+    }else{
+        printf("invalid data");
+    }
+
+    return 0;
+}
